b337de8e81b076eb1efb837d160bba74b5b34d39
drom98 <diogoomarques98@gmail.com>
2019-01-04 18:02:18 +0000

    Main layout

---
+++ src/components/sideBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <li>cenas</li>
+    <li>outras cenas</li>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'sideBar',
+}
+</script>
---
+++ src/views/Inicio.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-aside width="280px">
+
+      </el-aside>
+      <el-main>
+        
+      </el-main>
+      <div class="color-overlay"></div>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
